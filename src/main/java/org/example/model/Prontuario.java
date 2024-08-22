157a6423d9453d97e1e8d262ead94e9c401c5055
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-08-22 16:37:23 -0300

    Create Medicamentos.java and Prontuario.java with Update Veterinario.java

---
+++ src/main/java/org/example/model/Prontuario.java
@@ -0,0 +1,11 @@
+package org.example.model;
+
+import java.util.ArrayList;
+
+public class Prontuario {
+    private Cliente c;
+    private Pet p;
+    private Veterinario vet;
+    private ArrayList<Medicamentos> medicamentos;
+    private String descricao;
+}
--- src/main/java/org/example/model/Veterinario.java
+++ src/main/java/org/example/model/Veterinario.java
@@ -1,4 +1,27 @@
 package org.example.model;
 
-public class Veterinario {
+public class Veterinario extends Funcionario{
+
+    private String nConselho;
+
+    public Veterinario(String nome, char sexo, int idade, double salario, String nConselho) {
+        super(nome, sexo, idade, salario);
+        this.nConselho = nConselho;
+    }
+
+    public String getnConselho() {
+        return nConselho;
+    }
+
+    public void setnConselho(String nConselho) {
+        this.nConselho = nConselho;
+    }
+
+    public void avaliarPet(Pet p){
+
+    }
+
+    public void prescrever(Pet p){
+
+    }
 }
