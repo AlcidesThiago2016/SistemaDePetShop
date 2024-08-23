21faafc97f9a9cfd2bd2f897928767a06a8cc8fb
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-08-23 11:19:05 -0300

    Update BdPet.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,21 @@ import org.example.controller.BdPetShop;
 import org.example.model.Cliente;
 import org.example.model.Pet;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         BdPet BdPet = new BdPet();
 
-        Cliente c = new Cliente("Wilismar Passos",45,"04525698515", null);
-        Pet p = new Pet("Salsicha",4,'f',1.33, c);
-        c.setP(p);
+        //Cliente c = new Cliente("Wilismar Passos",45,"04525698515", null);
+        //Pet p = new Pet("Salsicha",4,'f',1.33, c);
+        //c.setP(p);
 
-        BdPet.cadastrar(p);
+        //BdPet.cadastrar(p);
+        ArrayList<Pet> pets = BdPet.getPets();
+
+        for (int i = 0; i < pets.size(); i++) {
+            System.out.println(pets.get(i).getNome());
+        }
     }
 }
--- src/main/java/org/example/controller/BdPet.java
+++ src/main/java/org/example/controller/BdPet.java
@@ -3,11 +3,18 @@ package org.example.controller;
 import org.example.model.Pet;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class BdPet {
 
-    public BdPet() {
+    private ArrayList<Pet> pets;
+    private ArrayList<String> linhas;
 
+    public BdPet() {
+        pets = new ArrayList<>();
+        linhas = new ArrayList<>();
+        ler();
+        pets = transformarEmObjetos(linhas);
     }
 
     public void cadastrar(Pet p) {
@@ -29,8 +36,6 @@ public class BdPet {
         } catch (Exception e){
             System.out.println("Erro ao abrir o arquivo!");
         }
-
-
     }
 
     public void alterar(Pet p) {
@@ -57,4 +62,70 @@ public class BdPet {
 
     }
 
+    public void ler(){
+        try {
+            InputStream is = new FileInputStream("cadastro_pet.txt");
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+
+            String linha = br.readLine();
+
+            while(linha != null){
+                System.out.println(linha);
+                linhas.add(linha);
+                linha = br.readLine();
+            }
+            br.close();
+            isr.close();
+            is.close();
+
+            System.out.println("Arquivo lido com sucesso!");
+        } catch (Exception e){
+            System.out.println("Erro ao ler o arquivo!");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public ArrayList<Pet> transformarEmObjetos(ArrayList<String> linhas){
+
+        String itens[] = new String[5];
+        Pet p;
+
+        String nome;
+        int idade;
+        char sexo;
+        double peso;
+        String cpf;
+
+        for(int i = 0; i < linhas.size(); i++){
+            itens = linhas.get(i).split(",");
+
+            nome = itens[0];
+            idade = Integer.parseInt(itens[1]);
+            sexo = itens[2].charAt(0);
+            peso = Double.parseDouble(itens[3]);
+            cpf = itens[4];
+
+            p = new Pet(nome,idade,sexo,peso,null);
+            pets.add(p);
+        }
+
+        return pets;
+    }
+
+    public ArrayList<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(ArrayList<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public ArrayList<String> getLinhas() {
+        return linhas;
+    }
+
+    public void setLinhas(ArrayList<String> linhas) {
+        this.linhas = linhas;
+    }
 }
